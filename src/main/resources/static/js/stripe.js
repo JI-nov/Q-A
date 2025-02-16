7cd78520a469f594620d112cee5a1092a293e8da
JI-nov <streetskabnd@icloud.com>
2025-02-17 06:12:59 +0900

    Delete src directory

--- src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = stripe('pk_test_51QrXT3Dvhv6106t5liv7Y4ehYtJ3QenW27w1Nre3tcvSJO5l0ZCyEZx57ZS4unBSSl2zOPkFPN1z0qiUnEUfPHU700RjIzRPFs');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
- stripe.redirectToCheckout({
-   sessionId: sessionId
- })
-});
