ce2737b0dd5c079323cfaaeeeb746c2fdc18c7f8
JI-nov <streetskabnd@icloud.com>
2025-03-03 12:33:00 +0900

    Add files via upload

--- src/main/java/com/example/samuraitravel/service/StripeService.java
+++ src/main/java/com/example/samuraitravel/service/StripeService.java
@@ -1,5 +1,8 @@
 package com.example.samuraitravel.service;
 
+import java.util.Map;
+import java.util.Optional;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -8,13 +11,22 @@ import org.springframework.stereotype.Service;
 import com.example.samuraitravel.form.ReservationRegisterForm;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Event;
+import com.stripe.model.StripeObject;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
+import com.stripe.param.checkout.SessionRetrieveParams;
 
 @Service
 public class StripeService {
 	@Value("${stripe.api-key}")
 	private String stripeApiKey;
+
+	private final ReservationService reservationService;
+
+	public StripeService(ReservationService reservationService) {
+		this.reservationService = reservationService;
+	}
 
 	// セッションを作成し、Stripeに必要な情報を返す
 	public String createStripeSession(String houseName, ReservationRegisterForm reservationRegisterForm,
@@ -57,5 +69,30 @@ public class StripeService {
 			e.printStackTrace();
 			return "";
 		}
+	}
+
+	// セッションから予約情報を取得し、ReservationServiceクラスを介してデータベースに登録する  
+	public void processSessionCompleted(Event event) {
+		Optional<StripeObject> optionalStripeObject = event.getDataObjectDeserializer().getObject();
+		optionalStripeObject.ifPresentOrElse(stripeObject -> {
+			Session session = (Session) stripeObject;
+			SessionRetrieveParams params = SessionRetrieveParams.builder().addExpand("payment_intent").build();
+
+			try {
+				session = Session.retrieve(session.getId(), params, null);
+				Map<String, String> paymentIntentObject = session.getPaymentIntentObject().getMetadata();
+				reservationService.create(paymentIntentObject);
+			} catch (StripeException e) {
+				e.printStackTrace();
+			}
+			System.out.println("予約一覧ページの登録処理が成功しました。");
+			System.out.println("Stripe API Version: " + event.getApiVersion());
+			System.out.println("stripe-java Version: " + Stripe.VERSION);
+		},
+				() -> {
+					System.out.println("予約一覧ページの登録処理が失敗しました。");
+					System.out.println("Stripe API Version: " + event.getApiVersion());
+					System.out.println("stripe-java Version: " + Stripe.VERSION);
+				});
 	}
 }
--- src/main/resources/static/js/stripe.js
+++ src/main/resources/static/js/stripe.js
@@ -1,4 +1,4 @@
-const stripe = stripe('pk_test_51QrXT3Dvhv6106t5liv7Y4ehYtJ3QenW27w1Nre3tcvSJO5l0ZCyEZx57ZS4unBSSl2zOPkFPN1z0qiUnEUfPHU700RjIzRPFs');
+const stripe = Stripe('pk_test_51QrXT3Dvhv6106t5liv7Y4ehYtJ3QenW27w1Nre3tcvSJO5l0ZCyEZx57ZS4unBSSl2zOPkFPN1z0qiUnEUfPHU700RjIzRPFs');
 const paymentButton = document.querySelector('#paymentButton');
 
 paymentButton.addEventListener('click', () => {
